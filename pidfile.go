c4b31464e15d469287f694f18f6570e8e5a79534
tanyanping <tanyanping@360.cn>
2016-09-28 14:49:56 +0800

    pusblish golang package for create and clear pid file

---
+++ pidfile.go
@@ -0,0 +1,91 @@
+package gopidfile
+
+import (
+	"errors"
+	"os"
+	"strconv"
+)
+
+const (
+	PidFileMinId         = 0
+	PidFileTmpPathSuffix = ".tmp"
+)
+
+type PidFile struct {
+	*Pid
+	*File
+
+	IsTmp      bool
+	OriginPath string
+}
+
+func CreatePidFile(path string) (*PidFile, error) {
+	file := NewFile(path)
+	isTmp := false
+
+	pid, err := ReadPidFromFile(file)
+	if err == nil && pid.ProcessExist() {
+		file = NewFile(path + PidFileTmpPathSuffix)
+		isTmp = true
+	}
+
+	pid = NewPid(os.Getpid())
+	if err := WritePidToFile(file, pid); err != nil {
+		return nil, err
+	}
+
+	return &PidFile{
+		Pid:        pid,
+		File:       file,
+		IsTmp:      isTmp,
+		OriginPath: path,
+	}, nil
+}
+
+func ClearPidFile(pidfile *PidFile) error {
+	pid, err := ReadPidFromFile(NewFile(pidfile.Path))
+	if err == nil && pidfile.Id == pid.Id {
+		if err = pidfile.Remove(); err != nil {
+			return err
+		}
+	}
+
+	if !pidfile.IsTmp {
+		tfile := NewFile(pidfile.Path + PidFileTmpPathSuffix)
+		tpid, err := ReadPidFromFile(tfile)
+		if err == nil && tpid.ProcessExist() {
+			if err = tfile.Rename(pidfile.Path); err != nil {
+				return err
+			}
+		}
+	} else {
+		ofile := NewFile(pidfile.OriginPath)
+		opid, err := ReadPidFromFile(ofile)
+		if err == nil && opid.Id == pidfile.Id {
+			if err = ofile.Remove(); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func ReadPidFromFile(file *File) (*Pid, error) {
+	fb, err := file.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := strconv.Atoi(string(fb))
+	if err != nil || id <= PidFileMinId {
+		return nil, errors.New("pid file data error")
+	}
+
+	return NewPid(id), nil
+}
+
+func WritePidToFile(file *File, pid *Pid) error {
+	fb := []byte(strconv.Itoa(pid.Id))
+	return file.Write(fb)
+}
